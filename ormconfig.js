484eb9c1b7f84b4bc8d95c0048deefe2da406cbb
AndreAquilau <andre.aquilau@outlook.com>
2020-10-20 21:54:53 -0300

    update

--- ormconfig.js
+++ ormconfig.js
@@ -3,9 +3,9 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  entities: [process.env.TYPEORM_ENTITIES || './dist/models/**/*.js'],
-  migrations: [process.env.TYPEORM_MIGRATIONS || './dist/models/**/*.js'],
-  subscribers: [process.env.TYPEORM_SUBSCRIBES || './dist/subscribers/**/*.js'],
+  entities: [process.env.TYPEORM_ENTITIES || 'dist/model/**/*.js'],
+  migrations: [process.env.TYPEORM_MIGRATIONS || 'dist/database/magrations/**/*.js'],
+  subscribers: [process.env.TYPEORM_SUBSCRIBES || 'dist/subscribers/**/*.js'],
   cli: {
     entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
     migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
